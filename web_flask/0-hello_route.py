c161f05a29d44c77f04abe063a920d42805cff4e
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-27 07:00:07 -0700

    -

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,6 @@ Routes:
 /: display “Hello HBNB!”
 You must use the option strict_slashes=False in your route definition
 '''
-
 from flask import Flask 
 
 app = Flask(__name__)
@@ -19,5 +18,5 @@ def hello_flask():
     return "Hello HBNB!"
 
 
-if __name__ == '__main':
-    app.run(host = '0.0.0.0', port = 5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
