b06940a227ee2cd696cfd35525abbea98dba6e2e
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-28 05:45:29 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,7 @@ def hbnb():
     return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_is_fun(text):
+def c(text):
     """ display “C ” followed by the value of the text variable 
     (replace underscore _ symbols with a space ) 
     """
