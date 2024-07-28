4052dc2c75bd753bf17c005ef080c13928a16a02
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-28 05:46:32 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,6 +16,7 @@ def hbnb():
     """ def doc """
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """ display “C ” followed by the value of the text variable 
